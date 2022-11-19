435907e72122a3de70dce88061c0871fdd2d2851
James Marshall <james@jamesbmarshall.com>
2022-11-19 20:58:26 +0000

    Site favicon

--- results.php
+++ results.php
@@ -2,6 +2,8 @@
     <head>
         <title>Azure Target Calculator</title>
         <link rel="stylesheet" type="text/css" href="style/style.css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik">
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     </head>
 
     <body>
