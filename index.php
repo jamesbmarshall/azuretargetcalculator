97da204d518587f6e2859217168035368748b3b8
James Marshall <james@jamesbmarshall.com>
2022-11-19 21:01:38 +0000

    Favicon fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <title>Azure Target Calculator</title>
         <link rel="stylesheet" type="text/css" href="style/style.css"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik">
-        <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
     </head>
 
     <body>
