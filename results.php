28c3d0aaea832bf46de3932022407f6874c0a1d4
James Marshall <jamesbmarshall@users.noreply.github.com>
2022-11-26 22:49:59 +0000

    Rounding

--- results.php
+++ results.php
@@ -183,7 +183,7 @@
             #echo number_format($i) . " ";
         }
 
-        return round($i, 2, PHP_ROUND_HALF_UP);
+        return round($i, 0, PHP_ROUND_HALF_UP);
     }
     
     function calcMarketingMetrics($customers) {
@@ -259,7 +259,7 @@ if ($targetMonthlyRevenue != null){
         <div class="column">
             <h2>Here are the results for your <?php echo $numberOfMonths ?> month plan!</h2>
             <h3>Summary</h3>
-            <p>Total number of new customers required: <span class="keypoint"><?php echo number_format(calcTotaliser($ACAMoM)) ?></span> consuming at least <span class="keypoint"><?php echo "$" . number_format($minAzureSpend) ?></span> per month.</p>
+            <p>Total number of new customers required: <span class="keypoint"><?php echo number_format(calcTotaliser($ACAMoM)) ?></span> consuming approximately <span class="keypoint"><?php echo "$" . number_format($minAzureSpend) ?></span> per month.</p>
             <p>Total ACR generated: <span class="keypoint">$<?php echo number_format(calcTotaliser($ACRTotal)) ?></span></p>
             <p>Annualised ACR at end of period: <span class="keypoint">$<?php echo number_format(round(($ACRTotal[$numberOfMonths - 1] * 12),0,PHP_ROUND_HALF_UP)) ?></span> ($<?php echo number_format(($ACRTotal[$numberOfMonths - 1])) . " * 12" ?>)</p>
             <h3>Details</h3>
