467b043fe41591525b25043581d4478729fec042
James Marshall <jamesbmarshall@users.noreply.github.com>
2022-11-26 13:44:38 +0000

    ACA total calc

--- results.php
+++ results.php
@@ -249,6 +249,8 @@ if ($targetMonthlyRevenue != null){
     calcGrowthACRMoM();
     calcGrowthACRTotal();
     calcMarketingMetrics(ceil(calcTotaliser($ACAMoM)));
+    $totalCustomers = number_format(round($ACATotal[$key],0,PHP_ROUND_HALF_UP)); 
+
 ?>
 <script src="scripts/d3-funnel.min.js"></script>
 <script src="scripts/d3-funnel.js"></script>  
@@ -257,7 +259,7 @@ if ($targetMonthlyRevenue != null){
         <div class="column">
             <h2>Here are the results for your <?php echo $numberOfMonths ?> month plan!</h2>
             <h3>Summary</h3>
-            <p>Total number of new customers required: <span class="keypoint"><?php echo number_format(ceil(calcTotaliser($ACAMoM))) ?></span> consuming at least <span class="keypoint"><?php echo "$" . number_format($minAzureSpend) ?></span> per month.</p>
+            <p>Total number of new customers required: <span class="keypoint"><?php echo $totalCustomers; ?></span> consuming at least <span class="keypoint"><?php echo "$" . number_format($minAzureSpend) ?></span> per month.</p>
             <p>Total ACR generated: <span class="keypoint">$<?php echo number_format(ceil(calcTotaliser($ACRTotal))) ?></span></p>
             <p>Annualised ACR at end of period: <span class="keypoint">$<?php echo number_format(($ACRTotal[$numberOfMonths - 1] * 12)) ?></span> ($<?php echo number_format(($ACRTotal[$numberOfMonths - 1])) . " * 12" ?>)</p>
             <h3>Details</h3>
