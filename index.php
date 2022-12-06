485c99588f0a39a6d2b34df587548b56513f47de
James Marshall <james@jamesbmarshall.com>
2022-12-06 22:56:40 +0000

    Formatting

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
             <input type="range" min="0" max="100" value="0" id="newbus" name="newbus" oninput="rangeValue.innerText = this.value + '%'"><span id="rangeValue">0%</span><br>
             <br>
             5. What is the <div class="tooltip_light">recurring revenue baseline<span class="tooltiptext">This is the monthly recurring revenue from the month immediately before this plan begins.</span></div> for the duration of your plan?<input type="number" id="mrrbaseline" name="mrrbaseline" placeholder="Example: 50000" required><br>
-            6. What is your <div class="tooltip_light">month-over-month growth rate<span class="tooltiptext">This is your estimated organic growth rate in percentage terms.</span></div>?<input type="number" min="0" max="100" id="momrate" name="momrate" placeholder="4" required><br>
+            6. What is your <div class="tooltip_light">month-over-month growth rate<span class="tooltiptext">This is your estimated organic growth rate in percentage terms.</span></div>?<input type="number" min="0" max="100" id="momrate" name="momrate" placeholder="1" required><br>
             <input type="submit" id="runcalc">
             </form>
             
