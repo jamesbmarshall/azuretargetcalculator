5213761de4b13658d2f7a90c9e9863f9a86851fe
James Marshall <james@jamesbmarshall.com>
2022-12-07 09:30:10 +0000

    Waterfall

--- results.php
+++ results.php
@@ -218,13 +218,9 @@ for ($x = 0; $x < $planLength; $x++)
                                 "total"
                             ],
                             x: [
-                                "Baseline",
-                                "New Business",
-                                "Baseline + New Business Total",
-                                "Baseline 'Organic' Growth",
-                                "New Business Growth",
-                                "Proactive Growth",
-                                "Total"
+                                ["Existing","Acquisition","Acquisition","Growth","Growth","Growth","Acquisition + Growth"],
+                                ["Baseline", "New Business", "Baseline + New Business Total", "Baseline 'Organic' Growth", "New Business Growth", "Proactive Growth", "Total"],
+                                
                             ],
                             textposition: "outside",
                             text: [
