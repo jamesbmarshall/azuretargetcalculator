b832e886b52d4f1f698caa79542a1b52ea84ae89
James Marshall <james@jamesbmarshall.com>
2022-12-08 10:30:37 +0000

    Glossary

--- glossary.php
+++ glossary.php
@@ -14,22 +14,31 @@
             important because you probably aren't starting from $0 and it's important to factor in your existing baseline when trying to calculate your targets. A $10,000 per month baseline equates to $120,000 of total revenue for the next year 
             (12 months * $10,000 of recurring revenue) - if your target is $240,000, then you've already got half of that 'in the bank'!</p>
 
+        <h1>O</h1>
+
+        <h2>Organic Growth</h2>
+
+            <p>One of the potential benefits of cloud services like Microsoft Azure is that end-customers can start to consume more services without the need to 'sell' anything new. This might be through simple scenarios like backing up more data, storing and
+            accessing more data, hosting more virtual desktops, etc. or it might be that a new department migrates into the cloud, uses a new application, and so on. This self-driven increase in consumption is referred to as <i>organic growth</i>, which is to say
+            that it happened without you needing to specifically do anything. It's not guaranteed, and some customers may not grow at all without help and support from you. However, over time you'll get an understanding of how much of your monthly growth
+            happens without you driving it. Therefore, a proportion of your monthly growth should be attributed to organic growth, the rest being what you add either through on-boarding new customers or proactively growing existing ones.</p>
+
         <h1>R</h1>
 
         <h2>Recurring Revenue</h2>
 
             <p>Recurring revenue is the amount of money you're receving each month from the sale and consumption of your services. This is different to traditional purchasing. For example, if I purchase a physical server costing $5,000 you'll get to bill
-            me once for $5,000. You might not sell me anything else for weeks, months, or even years. If, however, you're selling me something like a cloud service at $1,000 per month, you get to bill me every month that I use the service - this means
+            me once for $5,000. You might not sell me anything else for weeks, months, or even years. If, however, you're selling me a cloud service at $1,000 per month, you get to bill me every month that I use the service - this means
             you'd have $1,000 of <i>recurring revenue</i>. One of the biggest differences in sales between traditional 'up front' billing, and recurring billing, is how you approach hitting your targets.</p>
 
             <p>Let's say you've got 12 months and a target of $100,000 of revenue to generate. In a traditional model, where you're selling $5,000 physical servers, you'd need to sell 20 servers to hit your target. If you build your pipeline through the year,
             you could wait until the last day of the year and close all those deals and see all of that revenue immediately.</p>
 
-            <p>In the recurring revenue world, where you're selling $1,000 per month cloud services, you need to think differently. If you 'sell' one server in the first month of your year, it's worth $12,000 (12 months * $1,000 per month), if you sell one
-            in the second month, it's worth $11,000 (11 months * $1,000 per month). This continues to diminish until you reach the last month, where if you add your customer then it's worth only $1,000 (1 month * $1,000 per month). So, if you adopt the same 
-            approach of building pipeline and then looking to close deals at the end of the year, you'd need to add 100 customers in the last month of your year!</p>
+            <p>In the recurring revenue world, where you're selling $1,000 per month cloud services, you need to think differently. If you sell one 'server' in the first month of your year, it's worth $12,000 (12 months * $1,000 per month), if you sell one
+            in the second month, it's worth $11,000 (11 months * $1,000 per month). If you sell your 'server' in the last month then it's worth only $1,000 (1 month * $1,000 per month). If you leave closing deals at the end of the year, you'd need to add 
+            100 'servers' in the last month!</p>
 
-            <p>Instead, if you add one customer per month (1.3 to be exact in this example!), you'd only need to add 15.4 to generate $100,000 of revenue in the same 12 month period.</p>
+            <p>Instead, if you add one 'server' per month (1.3 to be exact in this example!), you'd only need to add 15.4 to generate $100,000 of revenue in the same 12 month period.</p>
 
         <br>
             <br>
