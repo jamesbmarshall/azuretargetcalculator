a2a4d71cef9b8546548fc97a488debf0b710e4e0
James Marshall <james@jamesbmarshall.com>
2023-10-17 11:02:05 +0100

    Text

--- results.php
+++ results.php
@@ -162,7 +162,7 @@ $annualisedRevenue = ($planArray[$planLength - 1][0][10] * 12);
         <div class="row white">
         <h2><span>🥳 </span>Here are the results for your <?php echo $planLength ?> month plan!</h2>
             <h3>Summary</h3>
-            <p class="tab"><span>🌱 </span>Total number of new customers required: <span class="keypoint"><?php echo number_format($planArray[$planLength - 1][0][8]) ?></span> consuming approximately <span class="keypoint"><?php echo "$" . number_format($targetSpend) ?></span> per month.</p>
+            <p class="tab"><span>🌱 </span>Total number of new customers required: <span class="keypoint"><?php echo number_format($planArray[$planLength - 1][0][8]) ?></span> consuming approximately <span class="keypoint"><?php echo "$" . number_format($targetSpend) ?></span> per month. $<?php echo number_format($newBusinessTotal) ?> total.</p>
             <p class="tab"><span>📈 </span>Amount of proactively driven growth from existing customers required: <span class="keypoint">$<?php echo number_format($proactiveGrowthTotal) ?></span></p>
             <p class="tab"><span>💵 </span>Total revenue generated: <span class="keypoint">$<?php echo number_format($totalRevenueGenerated) ?></span></p>
             <p class="tab"><span>🔁 </span>Annualised recurring revenue at end of plan: <span class="keypoint">$<?php echo number_format($annualisedRevenue) ?></span> ($<?php echo number_format($planArray[$planLength - 1][0][10]) . " * 12" ?>)</p>
